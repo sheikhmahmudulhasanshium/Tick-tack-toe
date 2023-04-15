c6bdcb36aa8d7f5721e1326bbeca6fc5c37b27b3
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2023-04-16 02:36:00 +0600

    basic design change

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -42,32 +42,3 @@ export default Settings
 
 
 
-
-/*import React from 'react'
-import { useTheme } from "next-themes";
-//import customSound from 'sounds/Click.mp3'
-//console.log(customSound)
-import {MdDarkMode,MdLightMode} from 'react-icons/md'
-
-const Sidebar = () => {
-  const { theme, setTheme } = useTheme();
-  let modeName=(theme==="light")?"Dark Mode":"Light Mode"
-  let modeSymbol=(theme==="light")?<MdDarkMode className='text-white'/>:<MdLightMode className='text-yellow-300'/>
-  const changeTheme = () => {
-    
-   setTheme(theme === "light" ? "dark" : "light");
-   
-  }
-  
-  return (
-    <div className='flex flex-col items-center justify-center'>
-      
-      <button onClick={changeTheme} className='flex items-center justify-center w-8/12 gap-3 px-5 py-2 my-2 text-black rounded-full bg-gradient-to-r from-slate-300 to-shiumcustom '>{modeName}{modeSymbol}</button>
-      <button  className='w-8/12 px-5 py-2 my-2 text-black rounded-full bg-gradient-to-r from-slate-300 to-shiumcustom'>Toggle Sound</button>
-      
-    </div>
-  )
-}
-
-export default Sidebar;
-*/
