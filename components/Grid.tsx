ab5ecf63702e6e307e3510ab19ee79e3898ebd59
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2023-04-19 07:09:55 +0600

    major update in game logic

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,68 +1,59 @@
 import React, { useState } from 'react'
-
+import {FaTrophy} from 'react-icons/fa'
+import {GrPowerReset} from 'react-icons/gr'
 export default function Grid()  {
     
     const[playerNo,setPlayerNo]=useState(1)
     const[countNo,setCountNo]=useState(0)
     const [inputValue,setInputValue]=useState([' ',' ',' ',' ',' ',' ',' ',' ',' '])
-    let [winner,setWinner]=useState(' ')
-    function gameLogic()
-    {
-        if(countNo>=4 && countNo<=9){
-           //Check colum wise values
-            //col-1
-            if(inputValue[0]===inputValue[3] && inputValue[3]===inputValue[6] && inputValue[0]===inputValue[6]){
-                if(inputValue[3]==="X"){setWinner("Player 1")} 
-                if(inputValue[3]==="O") {setWinner("Player 2")}
-            }
-            //col-2
-            else if(inputValue[1]===inputValue[4] && inputValue[4]===inputValue[7]&& inputValue[1]===inputValue[7]){
-                if(inputValue[7]==="X"){setWinner("Player 1")} 
-                if(inputValue[7]==="O") {setWinner("Player 2")}
-
-            }
-            //col-3
-            else if(inputValue[2]===inputValue[5] && inputValue[5]===inputValue[8] && inputValue[2]===inputValue[8]){
-                if(inputValue[2]==="X"){setWinner("Player 1")} 
-                if(inputValue[2]==="O") {setWinner("Player 2")}
-
-            }
+    const[winner,setWinner]=useState(' ')
+    const[isGameOver,setIsGameOver]=useState(false)
+    function resetGame(){
+        setInputValue([' ',' ',' ',' ',' ',' ',' ',' ',' ']);
+    }
+    function GridLogic(){
+        console.log(countNo)
+        //console.log(inputValue)
         
-                //Check row wise values
-                    //row-1
-            else if(inputValue[0]===inputValue[1] && inputValue[1]===inputValue[2] && inputValue[0]===inputValue[2]){
-                if(inputValue[1]==="X"){setWinner("Player 1")} 
-                if(inputValue[1]==="O") {setWinner("Player 2")}
-
-            }
-                    //row-2
-            else if(inputValue[3]===inputValue[4] && inputValue[4]===inputValue[5] && inputValue[3]===inputValue[5]){
-                if(inputValue[4]==="X"){setWinner("Player 1")} 
-                if(inputValue[4]==="O") {setWinner("Player 2")}
-
-            }
-                    //row-3
-            else if(inputValue[6]===inputValue[7] && inputValue[7]===inputValue[8] && inputValue[6]===inputValue[8]){
-                if(inputValue[7]==="X"){setWinner("Player 1")} 
-                if(inputValue[7]==="O") {setWinner("Player 2")}
-            }
-                    
-                        //check diagonal values
-            else if(inputValue[0]===inputValue[4] && inputValue[4]===inputValue[8] && inputValue[0]===inputValue[8]){
-                if(inputValue[4]==="X"){setWinner("Player 1")} 
-                if(inputValue[4]==="O") {setWinner("Player 2")}
-            }
-            else if(inputValue[2]===inputValue[4] && inputValue[4]===inputValue[6] && inputValue[2]===inputValue[6]){
-                if(inputValue[4]==="X"){setWinner("Player 1")} 
-                if(inputValue[4]==="O") {setWinner("Player 2")}
-            }
-            else{
-                            setWinner("Draw")
-                        }
-
-                    }                    
-                
+        //countNo range: 0-9
+        if(countNo===9||winner==='Player 1'||winner==='Player 2'||winner==='Draw'){
+            setIsGameOver(true)
+        }
+        if(countNo>=8&&winner!=='Player 1'&&winner!=='Player 2')
+        {
+           //setIsGameOver(true)
+            setWinner('Draw')
+        }
         
+        else{
+            //console.log(isGameOver) 
+            if(countNo>=4 && countNo<=8 &&winner!=='Player 1'&&winner!=='Player 2'){
+                for(let i=0;i<9;i++){
+                    //row
+                    if(inputValue[i]===inputValue[i+1]&&inputValue[i+1]===inputValue[i+2]&&inputValue[i+2]===inputValue[i]&&inputValue[i]!==' '){
+                        if(inputValue[i+2]==='X'){setWinner("Player 1"); break;}
+                        else if(inputValue[i+2]==='O'){setWinner("Player 2");break;}
+                        else{i=i+3}  
+                    }
+                    //col
+                    if(inputValue[i]===inputValue[i+3]&&inputValue[i+3]===inputValue[i+6]&&inputValue[i+6]===inputValue[i]&&inputValue[i]!==' '){
+                        if(inputValue[i+3]==='X'){setWinner('Player 1'); break;}
+                        else if(inputValue[i+3]==='O'){setWinner('Player 2');break;}
+                        else{i=i+1}
+                        
+                    }
+                }
+                //diagonal
+                if(inputValue[0]===inputValue[4]&&inputValue[4]===inputValue[8]&&inputValue[8]===inputValue[0]&&inputValue[0]!==' '){
+                    if(inputValue[4]==='X'){setWinner('Player 1')}
+                }
+                //reverse-diagonal
+                if(inputValue[2]===inputValue[4]&&inputValue[4]===inputValue[6]&&inputValue[6]===inputValue[2]&&inputValue[2]!==' '){
+                    if(inputValue[4]==='X'){setWinner('Player 1')}
+                }
+            }
+            
+        }
     }
     
     function changeValue(clickedIndex: number){
@@ -77,7 +68,7 @@ export default function Grid()  {
                 inputValue[clickedIndex]='O'
                 setInputValue(inputValue)}
         }
-        gameLogic()
+        GridLogic()
         //console.log(InputValue)
        //console.log(clickedIndex)
         
@@ -94,21 +85,54 @@ export default function Grid()  {
         'flex items-center justify-center w-20 h-20 text-5xl font-extrabold bg-gray-300 from-neutral-700 dark: text-slate-400 dark:bg-slate-900 hover:bg-lime-500 dark:hover:bg-lime-400' 
         key={index} onClick={()=>changeValue(index)}>{item}</div>}
         )
-        console.log(winner);
+        //console.log(winner);
   return (
-    
+    <div>
+    {//show game
+    (isGameOver===false)&&
     <div className='flex flex-col py-2 jusify-center'>
         {/*grid container*/}
-        <div className='grid grid-cols-3 gap-1 p-2 ' >
-            {gridItems}
-        </div>
-        {/**count and turn */}
-        <div className='flex-col px-2 py-3 font-bold bg-gray-400 text-slate-900 dark:text-white dark:bg-slate-800'>
-            <h3>Count: {countNo} </h3> 
-            <h3>Turn: Player {playerNo} </h3> 
-            <h3>Winner: {winner}</h3>
-        </div>
+            <div className='grid grid-cols-3 gap-1 p-2 ' >
+                {gridItems}
+            </div>
+            {/**count and turn */}
+            <div className='flex-col px-2 py-3 font-bold bg-gray-400 text-slate-900 dark:text-white dark:bg-slate-800'>
+                <h3>Count: {countNo} </h3> 
+                <h3>Turn: Player {playerNo} </h3> 
+                
+            </div>
     </div>
+  }
+  {//show result
+    (isGameOver===true && (winner==='Draw'||winner==='Player 1'||winner==='Player 2'))&&
+    <div className='flex justify-center p-4 my-4 bg-gray-400 item-center text-slate-900 dark:text-white dark:bg-slate-800 font-kaushan rounded-2xl'>
+        <div className='flex-col px-2 py-3 text-2xl '>
+            <h1 className='font-bold'>{`<<-----Game Over----->>`}</h1>
+            <div className='flex items-center justify-center space-x-2 '>
+                {(winner!=='Draw')&&
+                    <div>
+                        <h3>Winner </h3>
+                        <FaTrophy className='mx-6 text-yellow-500 '/>
+                        <h2>{winner}</h2>
+                   </div>
+                }
+                {(winner==='Draw')&&
+                    <div>
+                        <h3>Draw</h3>
+                    </div>
+                }
+                
+            </div>
+            <button className='flex items-center justify-center '>
+                <p>Reset</p><GrPowerReset/>
+            </button>
+        </div>
+                
+    </div>
+  }
+  </div>
   )
 }
+
+
 
